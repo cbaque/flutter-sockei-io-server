9494e94ffd96faad35d0039f54c0a3d262d20d87
Claudio Baque <cbaque@torresytorres.com>
2020-09-08 15:47:58 -0500

    listo para produccion

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,6 +1,20 @@
 const { io } = require('../index');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band('EROS RAMAZOTTI.') );
+bands.addBand( new Band('JULIO JARAMILLO') );
+bands.addBand( new Band('METALICA') );
+bands.addBand( new Band('QUEEN') );
+
+
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => { 
         console.log('Cliente desconectado')
     });
@@ -11,11 +25,26 @@ io.on('connection', client => {
         io.emit('mensaje', { admin: 'Nuevo Mensaje'  });
     });
 
-    client.on('emitir-mensaje',(payload) => {
-        console.log(payload);
+    // client.on('emitir-mensaje',(payload) => {
+    //     console.log(payload);
 
-        // io.emit('nuevo-mensaje', payload);  // EMITE A TODOS
-        client.broadcast.emit('nuevo-mensaje', payload);
-    });
+    //     // io.emit('nuevo-mensaje', payload);  // EMITE A TODOS
+    //     client.broadcast.emit('nuevo-mensaje', payload);
+    // });
+
+    client.on('vote-band',(payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });  
+    
+    client.on('add-band',(payload) => {
+        bands.addBand( new Band(payload.name));
+        io.emit('active-bands', bands.getBands());
+    });   
+
+    client.on('delete-band',(payload) => {
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands());
+    });     
 
 });
